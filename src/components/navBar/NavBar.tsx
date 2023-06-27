c440ea1d71b6c49c82a9f89e608fa3af8daad37e
Bishnu Pada Chanda <>
2023-06-28 02:48:16 +0600

    NavBar css style added

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,14 +1,17 @@
 import Link from 'next/link';
 import React from 'react';
 import {links} from './links';
+import styles from './NavBar.module.css';
 
 function NavBar() {
   return (
-    <div>
-      <Link href={'/'}>Demopedia</Link>
-      <div>
+    <div className={styles.container}>
+      <Link href={'/'} className={styles.logo}>
+        Demopedia
+      </Link>
+      <div className={styles.links}>
         {links.map((link) => (
-          <Link key={link.id} href={link.url}>
+          <Link key={link.id} href={link.url} className={styles.link}>
             {link.title}
           </Link>
         ))}
