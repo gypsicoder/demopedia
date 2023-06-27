c691864539a3c5c832c167a4bead1f5fe0c555cd
Bishnu Pada Chanda <>
2023-06-28 03:05:07 +0600

    Footer css style added with social icons

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import styles from './Footer.module.css';
+import Image from 'next/image';
 
 function Footer() {
   return (
-    <div>
+    <div className={styles.container}>
       <div>All rights reserved ©2023 Demopedia.</div>
+      <div className={styles.social}>
+        <Image
+          src={'/facebook.png'}
+          width={20}
+          height={20}
+          alt='facebook account'
+          className={styles.icon}
+        />
+        <Image
+          src={'/instagram.png'}
+          width={20}
+          height={20}
+          alt='instagram account'
+          className={styles.icon}
+        />
+        <Image
+          src={'/twitter.png'}
+          width={20}
+          height={20}
+          alt='twitter account'
+          className={styles.icon}
+        />
+        <Image
+          src={'/youtube.png'}
+          width={20}
+          height={20}
+          alt='youtube account'
+          className={styles.icon}
+        />
+      </div>
     </div>
   );
 }
