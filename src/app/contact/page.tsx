b759dfb1a46053f215365804089938d8fb6795a9
Bishnu Pada Chanda <>
2023-06-30 01:46:54 +0600

    Contact page content added

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import styles from './page.module.css';
+import Image from 'next/image';
+import UrlButton from '@/components/urlButton/UrlButton';
 
 function Contact() {
-  return <div>Contact Page</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>{"Let's Keep in Touch"}</h1>
+      <div className={styles.content}>
+        <div className={styles.imageContainer}>
+          <Image
+            src={'/contact.png'}
+            fill={true}
+            alt=''
+            className={styles.image}
+          />
+        </div>
+        <form action='' className={styles.form}>
+          <input type='text' className={styles.textInput} placeholder='Name' />
+          <input
+            type='email'
+            className={styles.textInput}
+            placeholder='Email'
+          />
+          <textarea
+            name='message'
+            id='message'
+            placeholder='Message'
+            cols={30}
+            rows={10}
+            className={styles.textArea}
+          ></textarea>
+          <UrlButton url='#' text='Send' />
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
