882d5c56a85cbfb062b02f81448367cd4d1c7521
Bishnu Pada Chanda <>
2024-07-27 00:54:34 +0600

    useSearchParams wrapped in a suspense boundary

--- src/app/dashboard/(auth)/login/page.tsx
+++ src/app/dashboard/(auth)/login/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, {FormEvent, useEffect, useState} from 'react';
+import React, {FormEvent, Suspense, useEffect, useState} from 'react';
 import {signIn, useSession} from 'next-auth/react';
 import {useRouter, useSearchParams} from 'next/navigation';
 import Link from 'next/link';
@@ -36,38 +36,46 @@ const Login = () => {
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>{success ? success : 'Welcome Back'}</h1>
-      <h2 className={styles.subtitle}>Please sign in to see the dashboard.</h2>
-      <form onSubmit={handleSubmit} className={styles.form}>
-        <input
-          type='email'
-          placeholder='Email'
-          className={styles.input}
-          required
-        />
-        <input
-          type='password'
-          placeholder='Password'
-          className={styles.input}
-          required
-        />
-        <button className={styles.button}>Login</button>
-        {error && error}
-      </form>
-      <button
-        onClick={() => {
-          signIn('google');
-        }}
-        className={styles.button + ' ' + styles.google}
-      >
-        Login with Google
-      </button>
-      <span className={styles.or}>- OR -</span>
-      <Link className={styles.link} href='/dashboard/register'>
-        Create new account
-      </Link>
-    </div>
+        <h1 className={styles.title}>{success ? success : 'Welcome Back'}</h1>
+        <h2 className={styles.subtitle}>Please sign in to see the dashboard.</h2>
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <input
+            type='email'
+            placeholder='Email'
+            className={styles.input}
+            required
+          />
+          <input
+            type='password'
+            placeholder='Password'
+            className={styles.input}
+            required
+          />
+          <button className={styles.button}>Login</button>
+          {error && error}
+        </form>
+        <button
+          onClick={() => {
+            signIn('google');
+          }}
+          className={styles.button + ' ' + styles.google}
+        >
+          Login with Google
+        </button>
+        <span className={styles.or}>- OR -</span>
+        <Link className={styles.link} href='/dashboard/register'>
+          Create new account
+        </Link>
+      </div>
   );
 };
 
-export default Login;
+const LoginPage = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Login />
+    </Suspense>
+  )
+}
+
+export default LoginPage;
