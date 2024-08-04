04409a40b5cd2989bc979c41504020d2233716e7
Bishnu Pada Chanda <>
2024-08-05 02:00:18 +0600

    Issue fixed: Portfolio images were not showing

--- src/components/portfolioItem/PortfolioItem.tsx
+++ src/components/portfolioItem/PortfolioItem.tsx
@@ -5,13 +5,13 @@ import Image from 'next/image';
 
 export const PortfolioItem = ({item}: {item: IPortfolioItem}) => {
   return (
-    <div className='flex gap-[50px] mt-[50px] mb-[100px] [&:nth-child(2n+1)]:flex-row-reverse'>
+    <div className='flex gap-[50px] mt-[50px] mb-[100px] odd:flex-row-reverse'>
       <div className='flex flex-col flex-1 justify-center gap-[20px]'>
         <h2 className='text-4xl'>{item.title}</h2>
         <p className='text-base'>{item.desc}</p>
         <UrlButton text='See More' url='#' />
       </div>
-      <div className='flex relative h-[500px]'>
+      <div className='flex-1 relative h-[500px]'>
         <Image src={item.image} alt='' fill={true} className='object-cover' />
       </div>
     </div>
